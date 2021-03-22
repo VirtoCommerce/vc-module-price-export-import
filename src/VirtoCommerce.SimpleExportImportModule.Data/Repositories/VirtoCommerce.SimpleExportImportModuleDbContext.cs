5065132f6fbbcd3a6053f05e458f6a286f3983e8
Anton Veretennikov <superiorboroda@gmail.com>
2021-03-22 16:37:03 +0300

    Remove commented code

--- src/VirtoCommerce.SimpleExportImportModule.Data/Repositories/VirtoCommerce.SimpleExportImportModuleDbContext.cs
+++ src/VirtoCommerce.SimpleExportImportModule.Data/Repositories/VirtoCommerce.SimpleExportImportModuleDbContext.cs
@@ -14,11 +14,9 @@ namespace VirtoCommerce.SimpleExportImportModule.Data.Repositories
             : base(options)
         {
         }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            //        modelBuilder.Entity<MyModuleEntity>().ToTable("MyModule").HasKey(x => x.Id);
-            //        modelBuilder.Entity<MyModuleEntity>().Property(x => x.Id).HasMaxLength(128);
-            //        base.OnModelCreating(modelBuilder);
         }
     }
 }
