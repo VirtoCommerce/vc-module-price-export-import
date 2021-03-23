fb08387576cc0c5b22e0c330cbcbb668807b2f22
Anton Veretennikov <superiorboroda@gmail.com>
2021-03-23 16:24:13 +0300

    Inject Import button to the prices list blade

--- src/VirtoCommerce.SimpleExportImportModule.Web/Scripts/module.js
+++ src/VirtoCommerce.SimpleExportImportModule.Web/Scripts/module.js
@@ -5,5 +5,18 @@ if (AppDependencies !== undefined) {
     AppDependencies.push(moduleName);
 }
 
-angular.module(moduleName, []);
+angular.module(moduleName, [])
+    .run(
+        ['platformWebApp.toolbarService',
+            function (toolbarService) {
+                toolbarService.register({
+                    name: "Import", icon: 'fa fa-download',
+                    executeMethod: function (blade) {
+                        console.log('test: ' + this.name + this.icon + blade);
+                    },
+                    canExecuteMethod: function () { return true; },
+                    index: 5
+                }, 'virtoCommerce.pricingModule.pricelistItemListController'); 
+            }
+        ]);
     
