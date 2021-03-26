778ab7a733acbf4d523b966da2e925f9d6e40f0c
Anton Veretennikov <superiorboroda@gmail.com>
2021-03-26 12:57:49 +0300

    VDS-1182: Create backend for csv pricing import preview (#9)
    * VDS-1182: Create backend for csv pricing import preview
    
    * Use using to dispose csvDataSource

---
+++ src/VirtoCommerce.SimpleExportImportModule.Core/Models/ImportDataPreview.cs
@@ -0,0 +1,9 @@
+using VirtoCommerce.Platform.Core.Common;
+using VirtoCommerce.PricingModule.Core.Model;
+
+namespace VirtoCommerce.SimpleExportImportModule.Core.Models
+{
+    public class ImportDataPreview : GenericSearchResult<ProductPrice>
+    {
+    }
+}
