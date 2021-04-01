463476f20b64611a03f3eb3aa1656bd54109e88c
Aleksandr Vishniakov <av@virtoway.com>
2021-04-01 14:26:32 +0200

    Fix variable name

--- src/VirtoCommerce.SimpleExportImportModule.Data/Services/CsvPagedPriceDataImporter.cs
+++ src/VirtoCommerce.SimpleExportImportModule.Data/Services/CsvPagedPriceDataImporter.cs
@@ -75,9 +75,9 @@ namespace VirtoCommerce.SimpleExportImportModule.Data.Services
                                 throw new NotImplementedException();
                             case ImportMode.CreateOnly:
                                 var validationResult = await _importProductPricesValidator.ValidateAsync(importProductPrices);
-                                var invalidProducts = validationResult.Errors.Select(x => (x.CustomState as ImportValidationState)?.InvalidImportProductPrice).Distinct().ToArray();
-                                importProgress.ErrorCount += invalidProducts.Length;
-                                importProductPrices = importProductPrices.Except(invalidProducts).ToArray();
+                                var invalidImportProductPrices = validationResult.Errors.Select(x => (x.CustomState as ImportValidationState)?.InvalidImportProductPrice).Distinct().ToArray();
+                                importProgress.ErrorCount += invalidImportProductPrices.Length;
+                                importProductPrices = importProductPrices.Except(invalidImportProductPrices).ToArray();
                                 createdPrices.AddRange(importProductPrices.Select(importProductPrice => importProductPrice.Price));
                                 break;
                             case ImportMode.UpdateOnly:
