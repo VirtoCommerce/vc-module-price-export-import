258a1913252a4fd4f3e34263c820374ff04e9af1
Ilya Litvinenko <ilya.ivanov6@gmail.com>
2021-04-01 10:21:06 +0200

    VDS-1196: Move simple export to simple-export-import module (#12)
    * VDS-1196: Move simple export to simple-export-import module
    
    * Change VirtoCommerce.Platform.Data version to 3.49.0
    
    * Remove unnecessary 'using'
    
    * Avoid using BuildServiceProvider
    
    * Fix after merge
    
    * Remove unnecessary using
    
    Co-authored-by: Dmitry Pushnitsa <d.pushnitsa@gmail.com>

---
+++ src/VirtoCommerce.SimpleExportImportModule.Web/Scripts/widgets/pricesWidget.js
@@ -0,0 +1,33 @@
+﻿angular.module('virtoCommerce.simpleExportImportModule')
+.controller('virtoCommerce.simpleExportImportModule.pricesWidgetController', ['$scope', 'platformWebApp.bladeNavigationService', 'virtoCommerce.pricingModule.prices', function ($scope, bladeNavigationService, prices) {
+    var blade = $scope.widget.blade;
+
+    function refresh() {
+        $scope.priceCount = '...';
+
+        prices.search({
+            priceListId: blade.currentEntityId,
+            take: 0
+        }, function (data) {
+            $scope.priceCount = data.totalCount;
+        });
+    }
+
+    $scope.openBlade = function () {
+        var newBlade = {
+            id: "simpleImportExportPricelistChild",
+            currency: blade.currentEntity.currency,
+            currentEntity: blade.currentEntity,
+            currentEntityId: blade.currentEntityId,
+            parentWidgetRefresh: refresh,
+            title: blade.title,
+            subtitle: 'pricing.blades.pricelist-item-list.subtitle',
+            controller: 'virtoCommerce.simpleExportImportModule.pricelistItemListController',
+            template: 'Modules/$(VirtoCommerce.SimpleExportImport)/Scripts/blades/pricelist-item-list.tpl.html'
+        };
+
+        bladeNavigationService.showBlade(newBlade, blade);
+    };
+
+    refresh();
+}]);
