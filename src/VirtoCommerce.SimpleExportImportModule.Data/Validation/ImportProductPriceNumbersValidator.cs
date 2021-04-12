0dc0f8a34e2e32de34a8893e66564817034b7080
Anton Veretennikov <superiorboroda@gmail.com>
2021-04-12 19:01:00 +0300

    Save the filed name into validation state for negative and empty sku cases

--- src/VirtoCommerce.SimpleExportImportModule.Data/Validation/ImportProductPriceNumbersValidator.cs
+++ src/VirtoCommerce.SimpleExportImportModule.Data/Validation/ImportProductPriceNumbersValidator.cs
@@ -15,13 +15,13 @@ namespace VirtoCommerce.SimpleExportImportModule.Data.Validation
         {
             RuleFor(importProductPrice => importProductPrice.Price.MinQuantity).GreaterThan(0)
                 .WithErrorCode(ModuleConstants.ValidationErrors.NegativeNumbers)
-                .WithState(importProductPrice => new ImportValidationState { InvalidImportProductPrice = importProductPrice });
+                .WithState(importProductPrice => new ImportValidationState { InvalidImportProductPrice = importProductPrice, FieldName = "Min quantity" });
             RuleFor(importProductPrice => importProductPrice.Price.List).GreaterThanOrEqualTo(0)
                 .WithErrorCode(ModuleConstants.ValidationErrors.NegativeNumbers)
-                .WithState(importProductPrice => new ImportValidationState { InvalidImportProductPrice = importProductPrice });
+                .WithState(importProductPrice => new ImportValidationState { InvalidImportProductPrice = importProductPrice, FieldName = "List price" });
             RuleFor(importProductPrice => importProductPrice.Price.Sale).GreaterThanOrEqualTo(0).When(importProductPrice => importProductPrice.Price.Sale != null)
                 .WithErrorCode(ModuleConstants.ValidationErrors.NegativeNumbers)
-                .WithState(importProductPrice => new ImportValidationState { InvalidImportProductPrice = importProductPrice });
+                .WithState(importProductPrice => new ImportValidationState { InvalidImportProductPrice = importProductPrice, FieldName = "Sale price" });
         }
     }
 }
