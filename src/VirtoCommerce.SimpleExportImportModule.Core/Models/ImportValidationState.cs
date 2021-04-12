55ab410dd86ff4707bab333a6ac50b5be335be6a
Anton Veretennikov <superiorboroda@gmail.com>
2021-04-12 19:01:00 +0300

    Save the filed name into validation state for negative and empty sku cases

--- src/VirtoCommerce.SimpleExportImportModule.Core/Models/ImportValidationState.cs
+++ src/VirtoCommerce.SimpleExportImportModule.Core/Models/ImportValidationState.cs
@@ -3,5 +3,7 @@ namespace VirtoCommerce.SimpleExportImportModule.Core.Models
     public class ImportValidationState
     {
         public ImportProductPrice InvalidImportProductPrice { get; set; }
+
+        public string FieldName { get; set; }
     }
 }
