81f989855289a6182e96ff4289e4b3e4c1dd3868
Aleksandr Vishniakov <av@virtoway.com>
2021-06-29 16:35:44 +0200

    V1.2.0 (#63)
    * VDS-1669: Fix wrong error message SKU does not exists (#48)
    
    * VDS-1380: "Upload new" button on preview blade (#49)
    
    * VDS-1379: Add Upload new button
    
    * Remove unused assets
    
    * Fix incorrect blade-closing method
    
    * Change Upload new button action in accordance with user-story
    
    * Rename 'Upload new' button in russian localization
    
    * VDS-1643: Rename vc-module-simple-export-import to vc-module-price-export-import (#50)
    
    * Rename project
    
    * Rename folders
    
    * Rename constants
    
    * Fix names
    
    * Rename file name
    
    * Rename file name
    
    * Rename repo in mkdocs.yml
    
    * Update main.yml
    
    * Update deploy-pr.yml
    
    * Rename localization key
    
    * Fix docs
    
    * Fix repo name
    
    * Add incompatibility
    
    * Increase module version
    
    * VDS-1720: Remove price widget from module (#58)
    
    * VDS-1720: Remove price widget from module
    
    * Use base function for advanced export
    
    * Create getPropetryInfo function
    
    * Rename function for propertyInfo creation
    
    * VDS-1634: Set environment up timeout to 15 (#59)
    
    * VDS-1786: Fix wrong text when Import price (#60)
    
    * VDS-1834: Add argoDeploy.json
    
    * Updating Github Action workflows.
    
    * Updating Github Action workflows.
    
    * VDS-1801: Fix Validation error message doesn't change when upload new file (#61)
    
    * 1.2.0
    
    Co-authored-by: Pavel Matantsev <kvissachaderah@gmail.com>
    Co-authored-by: Dmitry Pushnitsa <2745590@gmail.com>
    Co-authored-by: Ilya Litvinenko <ilya.ivanov6@gmail.com>
    Co-authored-by: Maksim Kopnov <44946644+mvktsk@users.noreply.github.com>
    Co-authored-by: Pavel Matantsev <matantsev.pn@gmail.com>
    Co-authored-by: Maksim <mko@virtoway.com>
    Co-authored-by: vc-ci <ci@virtocommerce.com>
    Co-authored-by: Dmitry Pushnitsa <d.pushnitsa@gmail.com>

--- src/VirtoCommerce.PriceExportImportModule.Web/Scripts/module.js
+++ src/VirtoCommerce.PriceExportImportModule.Web/Scripts/module.js
@@ -7,15 +7,135 @@ if (AppDependencies !== undefined) {
 
 angular.module(moduleName, [])
     .run(
-        ['virtoCommerce.featureManagerSubscriber', 'platformWebApp.widgetService', 'platformWebApp.authService',
-            function (featureManagerSubscriber, widgetService, authService) {
+        ['virtoCommerce.featureManagerSubscriber', 'platformWebApp.toolbarService', 'platformWebApp.bladeNavigationService', 'platformWebApp.dialogService', 'platformWebApp.settings',
+            function (featureManagerSubscriber, toolbarService, bladeNavigationService, dialogService, settings) {
                 featureManagerSubscriber.onLoginStatusChanged('PriceExportImport', () => {
 
-                    widgetService.registerWidget({
-                        isVisible: function (blade) { return blade.controller === 'virtoCommerce.pricingModule.pricelistDetailController' && !blade.isNew && authService.checkPermission('pricing:read'); },
-                        controller: 'virtoCommerce.priceExportImportModule.pricesWidgetController',
-                        template: 'Modules/$(VirtoCommerce.PriceExportImport)/Scripts/widgets/pricesWidget.tpl.html'
-                    }, 'pricelistDetail');
+                    toolbarService.register({
+                        name: "platform.commands.export", icon: 'fa fa-upload',
+                        executeMethod: function (blade) {
+                            const csvExportDelimiter = ';';
+                            const csvExportProvider = 'CsvExportProvider';
+                            const csvPropertyInfo = [
+                                createCsvPropertyInfo("Code", "SKU"),
+                                createCsvPropertyInfo("ProductName", "Product Name"),
+                                createCsvPropertyInfo("Currency", "Currency"),
+                                createCsvPropertyInfo("List", "List price"),
+                                createCsvPropertyInfo("Sale", "Sale price"),
+                                createCsvPropertyInfo("MinQuantity", "Min quantity"),
+                                createCsvPropertyInfo("ModifiedDate", "Modified"),
+                                createCsvPropertyInfo("StartDate", "Valid from"),
+                                createCsvPropertyInfo("EndDate", "Valid to"),
+                                createCsvPropertyInfo("CreatedDate", "Created date"),
+                                createCsvPropertyInfo("CreatedBy", "Created by"),
+                                createCsvPropertyInfo("ModifiedBy", "Modified By")
+                            ];
+                            const exportDataRequest = {
+                                exportTypeName: 'VirtoCommerce.PricingModule.Data.ExportImport.ExportablePrice',
+                                dataQuery: {
+                                    exportTypeName: 'PriceExportDataQuery'
+                                }
+                            };
+
+                            const selectedRows = blade.$scope.gridApi.selection.getSelectedRows();
+                            const isAllSelected = blade.$scope.gridApi.selection.getSelectAllState() || !selectedRows.length;
+                            exportDataRequest.dataQuery.isAllSelected = isAllSelected;
+                            exportDataRequest.dataQuery.objectIds = [];
+                            if (!isAllSelected && selectedRows) {
+                                const priceIds = _.pluck(_.flatten(_.pluck(selectedRows, 'prices')), 'id');
+                                exportDataRequest.dataQuery.objectIds = priceIds;
+                            }
+
+                            exportDataRequest.dataQuery.productIds = [];
+
+                            if ((exportDataRequest.dataQuery.productIds && exportDataRequest.dataQuery.productIds.length)
+                                || (!isAllSelected)) {
+                                exportDataRequest.dataQuery.productIds = _.map(selectedRows, function (product) {
+                                    return product.productId;
+                                });
+                            }
+
+                            const searchCriteria = blade.getSearchCriteria();
+
+                            if (isAllSelected || (searchCriteria.pricelistIds && searchCriteria.pricelistIds.length > 0) || searchCriteria.keyword !== '') {
+                                exportDataRequest.dataQuery.isAnyFilterApplied = true;
+                            }
+
+                            angular.extend(exportDataRequest.dataQuery, searchCriteria);
+
+                            const selectedItemsCount = isAllSelected ? blade.totalItems : selectedRows.length;
+                            settings.getValues({ id: 'PriceExportImport.Export.LimitOfLines' }, (value) => {
+                                const exportLimit = value[0];
+                                const validationError = selectedItemsCount > exportLimit;
+                                const dialog = {
+                                    id: "priceExportDialog",
+                                    exportAll: isAllSelected ? true : false,
+                                    totalItemsCount: blade.totalItems,
+                                    selectedItemsCount,
+                                    exportLimit: exportLimit,
+                                    validationError,
+                                    advancedExport: function () {
+                                        this.no();
+                                        blade.exportPrices();
+                                    },
+                                    callback: function (confirm) {
+                                        if (confirm) {
+                                            exportDataRequest.providerConfig = {};
+                                            exportDataRequest.providerConfig.delimiter = csvExportDelimiter;
+                                            exportDataRequest.providerConfig.type = 'CsvProviderConfiguration';
+                                            exportDataRequest.providerName = csvExportProvider;
+                                            exportDataRequest.dataQuery.includedProperties = csvPropertyInfo;
+                                            delete exportDataRequest.dataQuery.skip;
+                                            delete exportDataRequest.dataQuery.take;
+                                            delete exportDataRequest.dataQuery.IsPreview;
+                                            blade.isExporting = true;
+                                            const progressBlade = {
+                                                id: 'exportProgress',
+                                                title: 'export.blades.export-progress.title',
+                                                controller: 'virtoCommerce.exportModule.exportProgressController',
+                                                template: 'Modules/$(VirtoCommerce.Export)/Scripts/blades/export-progress.tpl.html',
+                                                exportDataRequest: exportDataRequest,
+                                                onCompleted: function () {
+                                                    blade.isExporting = false;
+                                                }
+                                            };
+                                            bladeNavigationService.showBlade(progressBlade, blade);
+                                        }
+                                    }
+                                }
+                                dialogService.showDialog(dialog, 'Modules/$(VirtoCommerce.PriceExportImport)/Scripts/dialogs/price-export-dialog.tpl.html', 'platformWebApp.confirmDialogController');
+                            });
+                        },
+                        canExecuteMethod: function () { return true; },
+                        index: 4
+                    }, 'virtoCommerce.pricingModule.pricelistItemListController');
+
+                    toolbarService.register({
+                        name: "platform.commands.import",
+                        icon: 'fa fa-download',
+                        executeMethod: function (blade) {
+                            const newBlade = {
+                                id: 'priceImportFileUpload',
+                                title: 'priceExportImport.blades.file-upload.title',
+                                subtitle: 'priceExportImport.blades.file-upload.subtitle',
+                                priceListId: blade.currentEntityId,
+                                controller: 'virtoCommerce.priceExportImportModule.fileUploadController',
+                                template: 'Modules/$(VirtoCommerce.PriceExportImport)/Scripts/blades/file-upload.tpl.html'
+                            };
+                            bladeNavigationService.showBlade(newBlade, blade);
+                        },
+                        canExecuteMethod: function () { return true; },
+                        index: 5
+                    }, 'virtoCommerce.pricingModule.pricelistItemListController');
+
+                    function createCsvPropertyInfo(fullName, displayName) {
+                        return {
+                            fullName,
+                            group: "TabularPrice",
+                            displayName,
+                            isRequired: false,
+                        }
+                    }
 
                 });
             }
